7447a032af3243c8941e8c11df34f96d1e6ab0ea
Viva <happybird00@naver.com>
2024-09-05 23:47:24 +0800

    Initial commit

---
+++ src/main/java/org/example/ticketingserviceflux/TicketingServiceFluxApplication.java
@@ -0,0 +1,13 @@
+package org.example.ticketingserviceflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingServiceFluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingServiceFluxApplication.class, args);
+    }
+
+}
